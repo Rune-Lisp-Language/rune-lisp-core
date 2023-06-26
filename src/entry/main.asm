bdcd2493f6404457efb50bf26f75896dad150825
SciBourne <bourne-sci-hack@yandex.ru>
2023-06-26 10:15:29 +0500

    FEAT: Add entry point

---
+++ src/entry/main.asm
@@ -0,0 +1,25 @@
+.section .text
+
+    .global _start
+    .global _exit
+    .global _exit_error
+
+    _start:
+	call print_title
+	call arg_handler
+	call print_parameters
+	call read_source
+
+    _exit:
+	mov sys_exit, %rax
+	mov return_normal, %rdi
+	syscall
+
+    _exit_error:
+	mov sys_write, %rax
+	mov stdout, %rdi
+	syscall
+
+	mov sys_exit, %rax
+	mov return_error, %rdi
+	syscall
